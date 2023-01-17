0148ed431b27959465b36f30d3f59b8eec52e752
Arturo Garrido Rosich <a.garridorosich@cifpfbmoll.eu>
2023-01-17 04:17:34 +0100

    fix: :bug: fix column name on foreign key

--- db/migrations/20230116212506_create_bike_identifications_table.js
+++ db/migrations/20230116212506_create_bike_identifications_table.js
@@ -5,8 +5,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('bike_identifications', function(table) {
       table.increments('id').primary();
-      table.integer('bike_models_id').unsigned().notNullable();
-      table.foreign('bike_models_id').references('bike_models.id');
+      table.integer('bike_model_id').unsigned().notNullable();
+      table.foreign('bike_model_id').references('bike_models.id');
       table.integer('business_id').unsigned().notNullable();
       table.foreign('business_id').references('business.id');
     });
