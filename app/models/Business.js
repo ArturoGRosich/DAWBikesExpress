5f082c627704952f81acf100ee1a1744e4c1c1f5
Arturo Garrido Rosich <a.garridorosich@cifpfbmoll.eu>
2023-01-17 03:14:50 +0100

    change ident to be space 4

--- app/models/Business.js
+++ app/models/Business.js
@@ -1,20 +1,20 @@
 const { Model } = require('objection');
 
 class Business extends Model {
-  static get tableName() {
-    return 'business';
-  }
+    static get tableName() {
+        return 'business';
+    }
 
-  static get jsonSchema() {
-    return {
-      type: 'object',
-      required: ['name'],
-      properties: {
-        id: { type: 'integer' },
-        name: { type: 'string', minLength: 1, maxLength: 255 },
-      }
-    };
-  }
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['name'],
+            properties: {
+                id: { type: 'integer' },
+                name: { type: 'string', minLength: 1, maxLength: 255 },
+            }
+        };
+    }
 }
 
 module.exports = Business;
