c13b7ef59e9c09e643bf0c06837ddede552a20eb
Arturo Garrido Rosich <a.garridorosich@cifpfbmoll.eu>
2023-01-17 04:17:48 +0100

    fix: :bug: fix column name on seeder

--- db/seeds/004-bike_identifications.js
+++ db/seeds/004-bike_identifications.js
@@ -7,15 +7,15 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('bike_identifications').insert([
         {
-          bike_models_id: 1,
+          bike_model_id: 1,
           business_id: 1
         },
         {
-          bike_models_id: 2,
+          bike_model_id: 2,
           business_id: 2
         },
         {
-          bike_models_id: 3,
+          bike_model_id: 3,
           business_id: 3
         }
       ]);
