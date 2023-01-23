32229928895f269951300e506edde69cf1680b41
Arturo Garrido Rosich <a.garridorosich@cifpfbmoll.eu>
2023-01-23 23:43:42 +0100

    created availabilityGet route

--- app/controllers/AvailabilityController.js
+++ app/controllers/AvailabilityController.js
@@ -3,12 +3,12 @@ const AvailabilityService = require('../services/AvailabilityService');
 class AvailabilityController {
     static async getAll(req, res) {
 
-        const bikeTypeId = req.query ? req.query['bike-type-id'] : null;
+        const bikeTypeId = req.query['bike-type-id'];
         const from = req.params.from;
         const to = req.params.to;
 
         try {
-            const Availability = await AvailabilityService.getAll(from , to, null);
+            const Availability = await AvailabilityService.getAll(bikeTypeId, from , to);
             res.status(200).json(Availability);
         } catch (error) {
             res.status(500).json({ message: error.message });
@@ -16,8 +16,10 @@ class AvailabilityController {
     }
     static async findById(req, res) {
         try {
-            const { id } = req.params;
-            const Availability = await AvailabilityService.findById(id);
+            const id = req.params.id;
+            const from = req.params.from;
+            const to = req.params.to;
+            const Availability = await AvailabilityService.findById(id, from , to);
             res.status(200).json(Availability);
         } catch (error) {
             res.status(500).json({ message: error.message });       
