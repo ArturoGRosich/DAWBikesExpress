f059fadfb80be7f9f0b868ce958742c327537025
Arturo Garrido Rosich <a.garridorosich@cifpfbmoll.eu>
2023-01-24 01:25:33 +0100

    fix:

--- db/migrations/20230116213314_create_bookings_table.js
+++ db/migrations/20230116213314_create_bookings_table.js
@@ -5,8 +5,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('bookings', function(table) {
       table.increments('id').primary();
-      table.dateTime('start').notNullable();
-      table.dateTime('finish').notNullable();
+      table.date('start').notNullable();
+      table.date('finish').notNullable();
       table.integer('bike_identification_id').unsigned().notNullable();
       table.foreign('bike_identification_id').references('bike_identifications.id');
     });
